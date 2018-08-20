a52364400ff5c4e6cc289e6e698489f2302a74d2
jc.bojaca <jc.bojaca@192.168.1.4>
2018-08-20 16:39:30 -0500

    yap

---
+++ Infracomp-Taller1/src/Punto2_RepartidorId.java
@@ -0,0 +1,86 @@
+
+public class Punto2_RepartidorId extends Thread {
+	private static int[][] matriz;
+	private static int maxTotal = 0;
+
+	private IDs id;
+	private boolean fin;
+
+	public Punto2_RepartidorId(IDs id) {
+		this.id = id;
+		this.fin = false;
+	}
+
+	@Override
+	public void run() {
+		int fil = id.getId();
+		while (fil != -1) {
+			int max = 0;
+			int[] fila = matriz[fil];
+			for (int i = 0; i < fila.length; i++) {
+				if (fila[i] > max)
+					max = fila[i];
+			}
+
+			System.out.println(this.getName() + ", fila " + fil + ": " + max);
+			max(max);
+			fil = id.getId();
+		}
+		fin = true;
+	}
+
+	public static void max(int val) {
+		if (val > maxTotal)
+			maxTotal = val;
+	}
+
+	public boolean termino() {
+		return fin;
+	}
+
+	public static void main(String[] args) {
+		int numeroFilas = 100;
+		crearMatriz(numeroFilas);
+		IDs ids = new IDs(numeroFilas);
+
+		int nprocesos = Runtime.getRuntime().availableProcessors();
+		Punto2_RepartidorId[] threads = new Punto2_RepartidorId[nprocesos];
+		for (int a = 0; a < nprocesos; a++)
+			(threads[a] = new Punto2_RepartidorId(ids)).start();
+
+		for (Punto2_RepartidorId sum : threads) {
+			while (!sum.termino())
+				;
+		}
+
+		System.out.println("el maximo es: " + maxTotal);
+		System.out.println("La respuesta debe ser: " + (numeroFilas * numeroFilas - 1));
+	}
+
+	private static void crearMatriz(int n) {
+		matriz = new int[n][n];
+		int valor = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matriz[i][j] = valor;
+				valor++;
+			}
+		}
+	}
+
+	static class IDs {
+		private int i = 0;
+		private final int filas;
+
+		public IDs(int filas) {
+			this.filas = filas;
+		}
+
+		public synchronized int getId() {
+			if (i < filas)
+				return i++;
+			return -1;
+		}
+	}
+}
---
+++ Infracomp-Taller1/src/Punto2_matrizRevez.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class Punto2_matrizRevez extends Thread {
+	private static int[][] matriz;
+	private static int maxTotal = 0;
+
+	private int id;
+	private boolean fin;
+
+	public Punto2_matrizRevez(int id) {
+		this.id = id;
+		this.fin = false;
+	}
+
+	@Override
+	public void run() {
+		int max = 0;
+		int[] fila = matriz[id];
+		for (int i = 0; i < fila.length; i++) {
+			if (fila[i] > max)
+				max = fila[i];
+		}
+		max(max);
+		fin = true;
+	}
+
+	public static void max(int val) {
+		if (val > maxTotal)
+			maxTotal = val;
+	}
+
+	public boolean termino() {
+		return fin;
+	}
+
+	public static void main(String[] args) {
+		int numeroFilas = 100;
+		crearMatriz(numeroFilas);
+
+		for(int[] a:matriz)
+			System.out.println(Arrays.toString(a));
+		
+		Punto2_matrizRevez[] threads = new Punto2_matrizRevez[numeroFilas];
+		for (int a = 0; a < numeroFilas; a++)
+			(threads[a] = new Punto2_matrizRevez(a)).start();
+
+		for (Punto2_matrizRevez sum : threads)
+			while (!sum.termino())
+				;
+
+		System.out.println("el maximo es: " + maxTotal);
+		System.out.println("La respuesta debe ser: " + (numeroFilas * numeroFilas - 1));
+	}
+
+	private static void crearMatriz(int n) {
+		matriz = new int[n][n];
+		int valor = 0;
+
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				matriz[i][j] = valor;
+				valor++;
+			}
+		}
+	}
+}
